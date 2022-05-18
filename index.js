6da34f66f99ac30ea226fec140b85078737e35c1
maxialbero <m.maffez@gmail.com>
2022-05-18 12:29:03 +0200

    ready to merge

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const app = express();
 app.use(require('body-parser').json());
 // prova
 //database = 'mongodb://localhost:27017/test'
-database = 'ciao'
 // register endpoint
-require('./api/index')(app, database);
+require('./api/index')(app);
+const bookSchema = new mongoose.Schema({
+    name: String
+});
+const Book = mongoose.model('Book', bookSchema);
+const private = new Book({ name: 'Private' });
 // connetti database
-mongoose.connect('mongodb://localhost:27017/test', (err, database) => {
+mongoose.connect('mongodb://localhost:27017/test', (err) => {
     if (err) return console.log(err)
     app.listen(8080, () => {
         console.log('server started!');
