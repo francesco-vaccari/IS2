6866e1076c1431d33c090d4f99ffcba982c2ef4e
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2022-05-22 19:12:54 +0200

    aggiunta logica signup

--- App express example/routes/signup.js
+++ App express example/routes/signup.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const { isNull } = require('url/util')
 const User = require('../models/User')
 
 router.get('/', (req, res) => {
-    res.render('signup')
+    res.render('signup', { message: req.query.message, username: req.query.username, password: req.query.password })
 })
 
 router.post('/', (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
+
+    const check = User.findOne({ "username": req.body.username}, 'username', (err, result) => {
+        if(isNull(result)){
+            const user = new User({
+                username: req.body.username,
+                password: req.body.password
+            })
+            user.save()
+            .then(data => {
+                res.redirect('/profile/' + req.body.username)
+            })
+        } else {
+            res.redirect('/signup?username=' + req.body.username + '&message=ERRORE: username già registrato')
+        }
     })
-    user.save()
-    .then(data => {
-        res.redirect('/profile/' + req.body.username)
-    })
+
     
 })
 
