31b503e2f4231ebde02254415f38b16ac50322a1
davidecaramori <davide.caramori@studenti.unitn.it>
2022-06-01 21:28:37 +0200

    modifiche api toruneys

--- App express example/api/v2/tourneys.js
+++ App express example/api/v2/tourneys.js
@@ -75,14 +75,47 @@ function validate(req){
     console.log("private ok")
     if(!Array.isArray(req.body.teams) || req.body.teams.length == 0){return false}
     console.log("teams ok")
-
+    
     return true
     
 }
 
+router.put('/', (req, res) => { //API per aggiungere giocatore al team di un torneo specifico
+    let teamId = [];
+    let playerId;
+    let arrTeams = [];
+    Tourney.findOne({"name": req.body.name}).populate("teams").exec((err, result) => { //cerca tutti i team corrispondenti al nome del torneo
+        if(err) { 
+            return handleError(err);
+        } else {
+            arrTeams = result.teams; //metto da parte i team trovati
+            for(let i=0; i<arrTeams.length; i++){ //cerco il nome del team che mi interessa tra i team di quel torneo
+                if(arrTeams[i].name==req.body.nameTeam) { 
+                    teamId[i] = arrTeams[i].id; // se trovo il team che mi interessa memorizzo il suo id
+                } 
+            } 
+        }
+        
+        Player.findOne({ name: req.body.playerName, surname: req.body.playerSurname }, (err, result) => { //cerco il player tramite il nome
+            if(err) { 
+                return handleError(err);
+            } else {
+                playerId = result.id; //mi tengo da parte l'id di quel player
+            }
+            Team.findOneAndUpdate( //inserisco il player al team che mi interessa
+            teamId,
+            { $push: { players: playerId } },
+            { upsert: true },
+            function(err, data) {
+            });
+        })
+    })
+    res.status(200).send();
+})
+
 /*
 router.get('/', (req, res) => {
-
+    
     const team1 = new Team({
         name: "team1"
     })
@@ -91,7 +124,7 @@ router.get('/', (req, res) => {
     })
     team1.save()
     team2.save()
-
+    
     const tourney = new Tourney({
         name: "torneo",
         startingDate: new Date("2022-06-01"),
@@ -101,10 +134,10 @@ router.get('/', (req, res) => {
         teams: [],
         games: []
     })
-
+    
     tourney.teams.push(team1)
     tourney.teams.push(team2)
-
+    
     tourney.save()
     .then(data => {
         console.log(tourney)
